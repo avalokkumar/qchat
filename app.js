f4ca20b65be1807030c7f9b992ec5f00d8916973
avalokkumar <avalokkumar@gmail.com>
2017-01-01 01:43:29 +0530

    socket.io integrated for chat communication

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 var express = require('express'),
+    app = express(),
+    server = require('http').createServer(app),
+    io = require('socket.io')(server),
+    port = process.env.port | 3000;
     path = require('path'),
     favicon = require('serve-favicon'),
     logger = require('morgan'),
-    cookieParser = require('cookie-parser'),
     bodyParser = require('body-parser'),
     index = require('./routes/index'),
     users = require('./routes/users'),
-    app = express();
+
 
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 /*app.set('views', path.join(__dirname, 'public/views')),*/
@@ -16,21 +19,39 @@ app.use(logger('dev')),
 app.use(bodyParser.json()),
 app.use(bodyParser.urlencoded({ extended: false })),
 
+io.on('connection', function (client) {
+    //console.log('Client Connected...');
+    client.on('join', function(name){
+        client.nickname = name;
+    })
+    client.on('messages', function (data) {
+        var nickname = client.nickname;
+        console.log(nickname +' Said : '+data);
+        client.broadcast.emit('message ', nickname+ ": "+data);
+        client.emit('messages', nickname+ ': '+data)
+    });
+});
 app.get("/", function(err, res) {
-    if(err){
+/*    if(err){
         console.log(err.message);
-    }
+    }*/
     res.sendFile(__dirname + "/public/views/index.html");
 });
+server.listen(port, function(){
+    console.log("Server listening to port "+port);
+});
+
+module.exports = app;
+
+
+
 
 // error handler
 /*app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});*/
-
-module.exports = app;
+ // set locals, only providing error in development
+ res.locals.message = err.message;
+ res.locals.error = req.app.get('env') === 'development' ? err : {};
+ // render the error page
+ res.status(err.status || 500);
+ res.render('error');
+ });*/
--- server.js
+++ server.js
@@ -1,5 +1,3 @@
-var http = require('http'), app = require('./app'), port = process.env.port | 3000;
 
-http.createServer(app).listen(port, function(){
-   console.log("Server listening to port "+port);
-});
+
+
